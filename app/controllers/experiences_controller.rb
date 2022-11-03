9dc4d2cae828ae49e01f2964bc5314c7b20c4e5b
JULES ROBINSON <julianna_85@icloud.com>
2022-11-03 14:39:34 -0500

    Experience create/showfinished. Update Action crash

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -11,10 +11,25 @@ class ExperiencesController < ApplicationController
       end_date: params[:end_date],
       degree: params[:degree],
       university_name: params[:university_name],
+      details: params[:details],
     )
     experience.save
     render json: experience.as_json
   end
-end
 
-# Finish controller http
+  def show
+    experience = Experience.find_by(id: params[:id])
+    render json: experience.as_json
+  end
+
+  def update
+    experience = Experience.find_by(is: params[:id])
+    experience.user_id = params[:user_id] || experience.user_id
+    experience.start_date = params[:start_date] || experience.start_date
+    experience.end_date = params[:end_date] || experience.end_date
+    experience.degree = params[:degree] || experience.degree
+    experience.university_name = params[:university_name] || experience.university_name
+    experience.details = params[:details] || experience.details
+    render json: experience.as_json
+  end
+end
