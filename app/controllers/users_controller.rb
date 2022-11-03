574e5358d05591c1c590771579bfd08f0a00f77d
Alan Garcia <alangarciacoding@gmail.com>
2022-11-02 19:51:03 -0500

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users.as_json
+  end
+
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password_digest: params[:password_digest],
+      phone_number: params[:phone_number],
+      linkedin_url: params[:linkedin_url],
+      personal_website_url: params[:personal_website_url],
+      online_resume_url: params[:online_resume_url],
+      github_url: params[:github_url],
+      image_url: params[:image_url],
+    )
+    user.save
+    render json: user.as_json
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user.as_json
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+    user.first_name = params[:first_name] || user.first_name
+    user.last_name = params[:last_name] || user.last_name
+    user.height = params[:height] || user.height
+    user.save
+    render json: user.as_json
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: { message: "User successfully destroyed." }
+  end
+end
